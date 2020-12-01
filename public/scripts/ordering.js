8574c00ad5e9732500f9dfc9c80b4eef3275554e
bnhughes <73972422+bnhughes@users.noreply.github.com>
2020-11-30 22:10:08 -0500

    Create ordering.js
    gime a sec, not done yet

---
+++ public/scripts/ordering.js
@@ -0,0 +1,68 @@
+
+function dispLog(log, n) {
+	console.log(n);
+	for (var i = 0; i < log.length; i++) {
+
+		let id = log[i][1][0].id;
+		$("#" + id).attr("style");
+
+		let temp = [log[i][0], log[i][1][0].innerText.split("-> ")[1].substr(0,30), log[i][1].attr("style")];
+		console.log(temp);
+		//console.log(log[i][1]);
+		if (i >= 1) {break;}
+	}
+}
+
+function isTheSameLogOrder(a, b) {
+	let len = a.length;
+	if (b.length != a.length) { return false; }
+
+	for (var i = 0; i < len; i++) {
+		/*console.log([a[i][0], a[i][1][0].innerText.split("-> ")[1].substr(0,30), +
+			b[i][0], b[i][1][0].innerText.split("-> ")[1].substr(0,30), +
+			a[i][0] != b[i][0]]);*/
+
+		if (a[i][0] != b[i][0]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+document.getElementById("draggableContainer").addEventListener("mouseleave", function() {
+	//console.log(this);
+	let container = $("#draggableContainer");
+	let eps = container.find(".draggableTile");
+
+	let log = [];
+
+	for (var i = 0; i < eps.length; i++) {
+		let ep = eps.eq(i);
+		let top = ep.position().top;
+		log.push([top, ep])
+		//if (i > 2) {break;}
+	}
+	//dispLog(log, "Original");
+	let oldlog = [];
+
+	for (var i = 0; i < log.length; i++) {
+		oldlog.push(log[i]);
+	}
+	log.sort(function(a, b) {return a[0] - b[0]});
+
+	if (isTheSameLogOrder(log, oldlog)) { oldlog = []; return; }
+	dispLog(log, "Exit Detected");
+	let newQueue = [];
+	for (var i = 0; i < log.length; i++) {
+		let id = log[i][1][0].id;
+		//newQueue.push([0, $("#" + id + "." + "draggableTile").clone().attr("style", "transform: matrix(1, 0, 0, 1, 0, -64);")]);
+		newQueue.push([0, $("#" + id + ".draggableTile").clone().attr("style", "")]);
+		//break;
+	}
+	dispLog(newQueue, "Edited style attribute");
+
+	let stylesheet = '<link rel="stylesheet" href="styles/draggableStyle.css" style="">';
+	$("#draggableContainer")[0].innerHTML = stylesheet;
+	//$("#draggableContainer").append(stylesheet);
+	//console.log($("#draggableContainer")[0].innerHTML);
+})
