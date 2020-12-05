762ebcc14bd5dc858dd431dbcd31a74549dd0b3c
wilwyler <wilwyler@umich.edu>
2020-12-05 11:37:20 -0500

    load more episodes

--- public/scripts/getEpisodes.js
+++ public/scripts/getEpisodes.js
@@ -2,7 +2,7 @@
 var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 // Block for individual episode info at the top of the center column
-var theShowBlock = `<div id="theShow-info" name="showID">
+var theShowBlock = `<div id="theShow-info" name="showID" numEps="(20)">
             <div id="theShow-pic">
                 <img src="show.gif" onerror=this.src="images/show.jpg">
             </div>
@@ -92,7 +92,8 @@ function getEpisodes(showID, showName, numberOfEpisodes, podcastBy, sortBy) {
 	    // Build upper block and update it onto the DOM
 		let coverBlock = theShowBlock.replace("show.gif", episodes[0].images[1].url) 
 			.replace("P-Title", showName)		.replace("(#) Songs", numberOfEpisodes + " Episodes")
-			.replace("(channel)", podcastBy)	.replace("showID", showID);
+			.replace("(channel)", podcastBy)	.replace("showID", showID)
+			.replace("(20)", numberOfEpisodes);
 			//.replace("unknown", priority)		.replace("nothing", sortOrder)
 
 		//console.log($("#" + showID).find(".dropdown"))
@@ -162,6 +163,130 @@ function getEpisodes(showID, showName, numberOfEpisodes, podcastBy, sortBy) {
 		
     }
   });
+}
+
+function loadMoreEpisodes() {
+	let currentPod = $("#theShow-info")[0].getAttribute("name");
+	let numEp = ($("#thePodcast").children().length)/2 + 0.5
+	let episodeList = $("#thePodcast").children();
+	let episode1Release, episode2Release;
+	episodeList.each(function(index) {
+		
+		if ((index == 2 || index == 0) && this.getAttribute("class") == "aPodcast") {
+			let episodeInfo = this.children;
+			let parts = episodeInfo[0].children;
+			
+				if (parts[2].getAttribute("id") == "tS-released") {
+					if (index == 0) {
+						episode1Release = parts[2].innerHTML;
+					} else {
+						episode2Release = parts[2].innerHTML;
+					}
+				}
+
+			
+		}
+	})
+
+	var date1 = new Date(episode1Release);
+	var date2 = new Date(episode2Release);
+	let totalEp = $("#theShow-info")[0].getAttribute("numeps")
+	let sendingURL = "https://api.spotify.com/v1/shows/" + currentPod + "/episodes?offset=";
+	//console.log(date1 < date2)
+	if (date1 < date2) {
+		// old to new
+		sendingURL += totalEp - numEp - 20;
+
+	} else {
+		// new to old
+		sendingURL += numEp;
+
+	}
+	
+	$("#thePodcast").append('<hr class="theCast-line" id="theCast-line">');
+	$.ajax({
+    	url: sendingURL,
+    	headers: {
+      	Authorization: "Bearer " + access_token,
+    	},
+    	success: function (response) {
+
+    		let episodes = response.items;
+
+    		
+    		// Add individual episode blocks to the podcast episodes container $(#"thePodcast")
+			let timer = 0; // stores the sum duration of all the podcast episodes loaded
+			let tempDate1 = new Date(episodes[0].release_date);
+			let tempDate2 = new Date(episodes[1].release_date);
+			console.log(date1 < date2)
+			console.log(tempDate1 < tempDate2)
+
+			if (date1 < date2) {
+				if (!(tempDate1 < tempDate2)) {
+					episodes.reverse();
+				}
+    		} else {
+    			if (tempDate1 < tempDate2) {
+    				episodes.reverse();
+    			}
+    		}
+
+    		tempDate1 = new Date(episodes[0].release_date);
+			tempDate2 = new Date(episodes[1].release_date);
+			console.log(tempDate1 < tempDate2)
+			episodes.forEach(function (episode) {
+	        	var currentEp = epBlock
+	        		.replace(" Titlehere", " " + episode.name)	.replaceAll("UNIQUEID", episode.id)
+	        		.replace("DESCRIPTION", episode.description)		.replace("RELEASED", numDate2strDate(episode.release_date))
+	        		.replace("DUR.", ms2time(episode.duration_ms))		.replace("T-UNIQUEID", "T-" + episode.id);
+
+	        	let shouldAppend = true;
+	        	$("#thePodcast").children().each(function(i) {
+	        		if (episode.id == this.getAttribute("id")) {
+	        			shouldAppend = false;
+	        		}
+	        	})
+	        	// Add new blocks to the DOM
+	        	if (shouldAppend) {
+	        		$("#thePodcast").append(currentEp);
+	        	
+	        // this next one sets the height of each episode block manually so that they can stack on each other while not leaving excess space in them
+	        		$("#" + episode.id).height($("#T-" + episode.id).height());
+	        		$("#" + episode.id).disableSelection();
+
+	        		timer += episode.duration_ms;
+	        	}
+	    	});
+	    // remove extra last hr elemnet
+	    // .theCast-line hr's r added with each block
+	    // one isnt needed after the last one for stylistic reasons
+	    	$(".theCast-line").eq($(".theCast-line").length - 1).remove();
+
+		// now that the total duration is known, update that in the title block
+
+			$("#theShow-info")[0].innerHTML = $("#theShow-info")[0].innerHTML.replace("(Total Play Length)", ms2time(timer));
+
+
+
+		// below: if an episode is in the queue, mark it with a green dot
+			let draggableEpisodes = $("#draggableContainer").find(".draggableTile");
+		//let picKey = episodes[0].images[2].url;
+
+		// goes throught the episodes in the queue containter $(#"draggableContainer")
+		// when an episode in that continer is in the podcast from the middle
+			// mark that middle podcast with a green dot
+			for (var i = 0; i < draggableEpisodes.length; i++) {
+				let draggableShowID = draggableEpisodes.eq(i).find(".dragCover").attr("id");
+
+				if (draggableShowID != currentPod) { continue; } // ignores episodes from other shows
+
+			// dispalys the green dot of that episode in the $(#"thePodcast") div
+				$("#" + draggableEpisodes.eq(i)[0].id).find("#green").css("display", "inline-block")
+			}
+
+    	}
+	})
+
 }
 
 $.fn.extend({
